ae060d0b8a578077d57ff416b1c172451a4a426f
lolizome <72571664+lolizome@users.noreply.github.com>
2026-02-13 12:58:16 +0100

    refactor(navbar): extract UserButton and implement loading states

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={`${outfit.className} antialiased text-gray-700`} >
           <AppContextProvider>
             <Toaster />
